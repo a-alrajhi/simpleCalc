99776f41592eeef42b0662bcfa467b340d7c4bb5
Doom <a_alrajhi@rocketmail.com>
2025-10-07 19:23:30 +0300

    tests added

---
+++ src/main/java/com/app/model/Calculator.java
@@ -0,0 +1,22 @@
+package com.app.model;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Calculator {
+
+  public static int add(int a, int b) {
+    return a + b;
+  }
+
+  public static int sub(int a, int b) {
+    return a - b;
+  }
+
+  public static int mul(int a, int b) {
+    return a * b;
+  }
+
+  public static int div(int a, int b) {
+    return a / b;
+  }
+}
